8058e180fde28bdad6d72b4faf8defe16c141ec1
tbro <tbro@users.noreply.github.com>
2023-10-12 11:35:33 -0500

    compiles

---
+++ host/src/main.rs
@@ -0,0 +1,29 @@
+use hello_risczero_methods::{MULTIPLY_ELF, MULTIPLY_ID};
+use risc0_zkvm::{default_prover, ExecutorEnv};
+
+fn main() {
+    // First, we construct an executor environment
+    let env = ExecutorEnv::builder().build().unwrap();
+
+    // TODO: add guest input to the executor environment using
+    // ExecutorEnvBuilder::add_input().
+    // To access this method, you'll need to use the alternate construction
+    // ExecutorEnv::builder(), which creates an ExecutorEnvBuilder. When you're
+    // done adding input, call ExecutorEnvBuilder::build().
+
+    // For example:
+    // let env = ExecutorEnv::builder().add_input(&vec).build().unwrap();
+
+    // Obtain the default prover.
+    let prover = default_prover();
+
+    // Produce a receipt by proving the specified ELF binary.
+    let receipt = prover.prove_elf(env, MULTIPLY_ELF).unwrap();
+
+    // TODO: Implement code for transmitting or serializing the receipt for
+    // other parties to verify here
+
+    // Optional: Verify receipt to confirm that recipients will also be able to
+    // verify your receipt
+    receipt.verify(MULTIPLY_ID).unwrap();
+}
